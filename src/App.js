8dafa484bfd4779682969ebc0cca8115855a7910
Philip Loa <boyet007@gmail.com>
2019-08-29 16:54:46 +0700

    Finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Todo from './Todo'
+import TodoForm from './TodoForm'
 import './App.css';
 
+
 function App() {
+  const [todos, setTodos] = useState([
+    { text: 'Learn about React', isComplete: false},
+    { text: 'Meet friend for lunch', isComplete: false},
+    { text: 'Build Really cool todo app', isComplete: false},
+  ])
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }]
+    setTodos(newTodos)
+  }
+
+  const completeTodo = index => {
+    const newTodos = [...todos]
+    newTodos[index].isComplete = true
+    setTodos(newTodos)
+  }
+
+  const removeTodo = index => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo-list">
+        { todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo}
+          completeTodo={completeTodo}
+          removeTodo={removeTodo}/>          
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
   );
 }
---
+++ src/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todo = ({ todo, index, completeTodo, removeTodo }) => <div className="todo" style={{ textDecoration: todo.isComplete ? "line-through" : "" }}>
+    { todo.text }
+    <div>
+        <button onClick={()=> completeTodo(index)}>Complete</button>
+        <button onClick={()=> removeTodo(index)}>x</button>
+    </div>
+</div>
+
+export default Todo
